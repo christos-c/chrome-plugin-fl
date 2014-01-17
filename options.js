53ed806155075b9abf92759541fdf9ffd40db102
Christos Christodoulopoulos <christos.c2009@gmail.com>
2014-01-16 23:26:55 -0600

    Added refresh button functionality Added refresh (success/fail) message

--- options.js
+++ options.js
@@ -2,20 +2,11 @@
 function save_options() {
   var delay = document.getElementById("delay").value;
   localStorage["delay_time"] = delay;
-  //document.getElementById('shim').style.display=
-  document.getElementById('msgbx').style.display ="block";
+  document.getElementById('msgSaved').style.display ="block";
   //To hide
-  //document.getElementById('shim').style.display=document.getElementById('msgbx').style.display ="none";
   setTimeout(function() {
-	//document.getElementById('shim').style.display=
-	document.getElementById('msgbx').style.display ="none";
+	document.getElementById('msgSaved').style.display ="none";
   }, 1250);
-  // Update status to let user know options were saved.
-  //var status = document.getElementById("status");
-  //status.innerHTML = "Options Saved.";
-  //setTimeout(function() {
-  //  status.innerHTML = "";
-  //}, 1500);
 }
 
 // Restores select box state to saved value from localStorage.
@@ -28,3 +19,99 @@ function restore_options() {
 }
 document.addEventListener('DOMContentLoaded', restore_options);
 document.querySelector('#save').addEventListener('click', save_options);
+document.querySelector('#refresh').addEventListener('click', startRequest);
+
+// REPLICATED CODE FROM background.js SINCE I DON'T KNOW OF A BETTER WAY
+function startRequest() {
+	console.log('Statring request');
+    document.getElementById('msgRefresh').style.display ="block";
+	// We'll hide it after request is done
+	getActionCount(
+		function(response) {
+			document.getElementById('msgRefresh').style.display ="none";
+			console.log('Request success');
+			updateIcon(response);
+		},
+		function() {
+			document.getElementById('msgRefresh').style.display ="none";
+			document.getElementById('msgRefreshFail').style.display ="block";
+			setTimeout(function() {
+				document.getElementById('msgRefreshFail').style.display ="none";
+			}, 1250);
+			console.log('Error during request');
+			updateIcon();
+		}
+	);
+}
+
+function createCORSRequest(method, url) {
+	var xhr = new XMLHttpRequest();
+	if ("withCredentials" in xhr) {
+		xhr.open(method, url, true);
+	} else {
+		// CORS not supported.
+		xhr = null;
+	}
+	return xhr;
+}
+
+function isGameUrl(url) {
+	return url.indexOf(getGameUrl()) == 0;
+}
+
+function getGameUrl() {
+	return 'http://fallenlondon.storynexus.com/Gap/Load?content=%2fStorylet%2fAvailable';
+}
+
+function getActionCount(onSuccess, onError) {
+	console.log('Getting action count...');
+	var xhr = createCORSRequest('GET', getGameUrl());
+	
+	function handleSuccess(response) {
+		if (onSuccess)
+			onSuccess(response);
+	}
+
+	var invokedErrorCallback = false;
+	function handleError() {
+		if (onError && !invokedErrorCallback)
+			onError();
+		invokedErrorCallback = true;
+	}
+
+	xhr.onload = function() {
+		if (xhr.responseText) {
+			var response = xhr.responseText;
+			if (response) {
+				handleSuccess(response);
+				return;
+			}
+		}
+		handleError();
+	};
+
+	xhr.onerror = function(error) {
+		handleError();
+	};
+
+	xhr.send();
+}
+
+function updateIcon(response) {
+	if (!response || !response.match('<span id="infoBarCurrentActions">([0-9]*)?</span>')) {
+		//Need to login
+		chrome.browserAction.setTitle({title: 'Fallen London: Not logged in! Click here.'});
+		chrome.browserAction.setBadgeText({text: ''});
+		chrome.browserAction.setIcon({path: 'hat-logo-inactive-19.png'});
+	}
+	else {
+		var count = response.match('<span id="infoBarCurrentActions">([0-9]*)?</span>')[1];
+		console.log('Actions: ' + count);
+		chrome.browserAction.setTitle({title: 'Fallen London: '+count+' available actions.'});
+		chrome.browserAction.setIcon({path: 'hat-logo-active-19.png'});
+		if (count == 0)
+			chrome.browserAction.setBadgeText({text: ''});
+		else
+			chrome.browserAction.setBadgeText({text: count});
+	}
+}
